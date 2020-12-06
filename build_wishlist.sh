79f2e4b28a658973c22a8432c7d21927cdee1d2c
Kevin Dion <kdion7@gmail.com>
2020-12-06 08:48:03 -0500

    Built Wishlist

---
+++ build_wishlist.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+BUILT_WISHLIST_FILE="built_wishlist.txt"
+BUILT_WISHLIST_TITLE="A compiled wishlist generated from sublists"
+
+MKB=true
+CONTROLLER=false
+INCLUDE_TRASH=true
+
+add_sublist() {
+    echo "Adding $1"
+
+    cat $1 >> $BUILT_WISHLIST_FILE
+    echo $'\n' >> $BUILT_WISHLIST_FILE
+}
+
+echo "Combining sublists into ${BUILT_WISHLIST_FILE}:"
+
+echo "title:${BUILT_WISHLIST_TITLE}" > $BUILT_WISHLIST_FILE
+echo $'\n' >> $BUILT_WISHLIST_FILE
+
+# Veritas1000's lists
+for sublist in Veritas1000/*.txt;do
+    add_sublist $sublist
+done
+
+#Panda's lists
+for sublist in PandaPaxxy/*.txt;do
+    if [[ $sublist == *"controller"* && "$CONTROLLER" = false ]] \
+        || [[ $sublist == *"mkb"* && "$MKB" = false ]]
+    then
+        echo "-- SKIPPING $sublist"
+    else
+        add_sublist $sublist
+    fi
+done
+
+# Mercules904's lists
+for sublist in Mercules904/*.txt;do
+    add_sublist $sublist
+done
+
+# AyyItsChevy's lists
+for sublist in AyyItsChevy/*.txt;do
+    add_sublist $sublist
+done
+
+if [ "$INCLUDE_TRASH" = true ];then
+    add_sublist misc/trash_rolls.txt
+fi
