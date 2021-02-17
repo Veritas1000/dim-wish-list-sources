c702838ed940150683c73ce7995628cdc200e9db
Kevin Dion <kdion7@gmail.com>
2021-02-17 09:59:34 -0500

    Added personal trash rolls and new scout rolls

--- build_wishlist.sh
+++ build_wishlist.sh
@@ -11,13 +11,13 @@ add_sublist() {
     echo "Adding $1"
 
     cat $1 >> $BUILT_WISHLIST_FILE
-    echo $'\n' >> $BUILT_WISHLIST_FILE
+    echo "" >> $BUILT_WISHLIST_FILE
 }
 
 echo "Combining sublists into ${BUILT_WISHLIST_FILE}:"
 
-echo "title:${BUILT_WISHLIST_TITLE}" > $BUILT_WISHLIST_FILE
-echo $'\n' >> $BUILT_WISHLIST_FILE
+echo "title: ${BUILT_WISHLIST_TITLE}" > $BUILT_WISHLIST_FILE
+echo "" >> $BUILT_WISHLIST_FILE
 
 # Veritas1000's lists
 for sublist in "$(ls -1 Veritas1000/*.txt | sort -r)";do
@@ -46,5 +46,7 @@ for sublist in AyyItsChevy/*.txt;do
 done
 
 if [ "$INCLUDE_TRASH" = true ];then
-    add_sublist misc/trash_rolls.txt
+    for sublist in "$(ls -1 */*trash*.txt | sort -r)";do
+        add_sublist "$sublist"
+    done
 fi
